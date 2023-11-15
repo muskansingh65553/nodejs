42644ab7820a665353fa399741e83196a90e83c1
muskansingh65553 <71725061+muskansingh65553@users.noreply.github.com>
2023-11-15 16:23:45 +0530

    aa

---
+++ purgecss.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    content: ['./views/**/*.ejs', './public/js/**/*.js'], // Paths to your content files
+    css: ['./public/css/**/*.css'], // Paths to your CSS files
+    // Other options and settings
+  };
+  
